21c1a00fec8a9d6ff61d070e24e052f7e47fbfdd
esel-fliegen <esel.fliegen@gmail.com>
2024-03-01 22:13:01 +0700

    user

--- src/main/java/com/synergisticit/controller/UserController.java
+++ src/main/java/com/synergisticit/controller/UserController.java
@@ -8,30 +8,48 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-@Controller
+@CrossOrigin(origins = "http://localhost:27016")
+@RestController
+@RequestMapping("users")
+
 public class UserController {
 
     @Autowired
     UserService userService;
 
 
-    @RequestMapping("saveUser")
-    public ResponseEntity<?> savesUser(@RequestParam String userID, @RequestParam String userName,
-                                       @RequestParam String password, @RequestParam String email){
+    @PostMapping("saveUser")
+    public ResponseEntity<User> savesUser(@RequestBody User user){
 
-
-        User user = new User(userID, userName, password, email);
-        userService.save(user);
-        return new ResponseEntity<>(user.toString(), HttpStatus.CREATED);
+        User savedUser = userService.save(user);
+        return new ResponseEntity<User>(savedUser, HttpStatus.CREATED);
     }
 
-    @RequestMapping("findAll")
+    @PostMapping("finduser")
+    public  ResponseEntity<?> findUser(@RequestBody User user){
+
+        if(user.getUserID() != null){
+
+            User foundUser = userService.findById(user.getUserID());
+            if(foundUser != null){
+
+                return new ResponseEntity<User>(foundUser, HttpStatus.FOUND);
+            } else {
+                //User savedUser = userService.save(user);
+
+                return new ResponseEntity<String>("User not found", HttpStatus.NOT_FOUND);
+            }
+        } else {
+            return null;
+        }
+    }
+    @GetMapping("findAll")
     public  ResponseEntity<List<User>> findAllUsers(){
 
         return new ResponseEntity<List<User>>(userService.findAll(), HttpStatus.FOUND);
